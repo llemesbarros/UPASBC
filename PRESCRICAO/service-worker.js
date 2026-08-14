d87852f2ee9f44c2878b1d8331812787124dbaca
llemesbarros <llemesbarros@gmail.com>
2026-08-14 12:52:35 -0300

    Keep evolution cache when prescription updates

--- PRESCRICAO/service-worker.js
+++ PRESCRICAO/service-worker.js
@@ -18,7 +18,13 @@ self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches
       .keys()
-      .then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))))
+      .then((keys) =>
+        Promise.all(
+          keys
+            .filter((key) => key.startsWith('prescricao-medica-pwa-') && key !== CACHE_NAME)
+            .map((key) => caches.delete(key)),
+        ),
+      )
       .then(() => self.clients.claim()),
   );
 });
